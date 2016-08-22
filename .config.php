9e1e3e3ca8b9a986dab43237b434e1b3a4f1c166
lykshin <lykshin.alexander@gmail.com>
2016-08-22 21:35:08 +0300

    v 1.0

---
+++ .config.php
@@ -0,0 +1,11 @@
+<?php
+
+    define("CORE", "XML");
+    //define("CORE", "MySQL");
+    define("CLIENT_ID_LENGTH", 8);
+
+    // MySQL Config
+    define("MYSQL_HOST", "localhost");
+    define("MYSQL_BASE", "sandbox");
+    define("MYSQL_USER", "user");
+    define("MYSQL_PASS", "pass");
